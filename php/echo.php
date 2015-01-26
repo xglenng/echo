78ce425449d239677dcf3a5888faa0f4d471711f
xglenng <xglenng@gmail.com>
2015-01-26 14:26:46 -0700

    Commit for echo files

--- php/echo.php
+++ php/echo.php
@@ -1,10 +1,14 @@
 <?php
-
-function echo()
+function echo1()
 {
-  // ...
+    for($i=0;$i<func_num_args();$i++)
+    {
+        $argument = func_get_arg($i);
+        echo"<script>console.log('$argument')</script>";
+        echo "<h4>$argument</h4>";
+    }
 }
-echo "Testing";
-echo();
-echo('bla');
-echo('foo', 'bar', 'baz');
+echo1();
+echo1('bla');
+echo1('foo', 'bar', 'baz');
+?>
